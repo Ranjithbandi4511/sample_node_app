d205569fdcc58a3efdb1105bed7aac970d004c6c
BANDI RANJITH KUMAR <ranjithkumarbandi004@gmail.com>
2025-05-04 19:06:49 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello from Railway!'));
+app.get('/', (req, res) => res.send('Hello from Railway! 2'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
