51fe81acab1ccbaeeb810f32fabc6003c2e30f8e
BANDI RANJITH KUMAR <ranjithkumarbandi004@gmail.com>
2025-05-05 05:43:11 +0530

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hai Mai'));
+app.get('/', (req, res) => res.send('test Node server'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
